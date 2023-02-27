cb1964fea557015828b6df2ec5198146cedd8023
chris-windsor <ccw3102@icloud.com>
2023-02-26 19:16:37 -0700

    Added Inventory impl

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ struct Item {
 #[derive(Clone)]
 struct Inventory {
     items: HashMap<String, Item>,
+    held: HashMap<u8, Vec<Item>>,
 }
 
 #[derive(Clone)]
@@ -19,9 +20,57 @@ struct Order {
     items: Vec<Item>,
 }
 
+impl Inventory {
+    fn hold_items(&mut self, order: &Order) -> bool {
+        let mut clone_inventory = self.items.clone();
+        for order_item in &order.items {
+            match clone_inventory.get_mut(&order_item.name) {
+                Some(item) => {
+                    if item.qty >= order_item.qty {
+                        item.qty -= order_item.qty;
+                    } else {
+                        println!(
+                            "| Order id{} over requested\n -{} has qty of {} but order is requesting qty of {}",
+                            order.id, item.name, item.qty, order_item.qty
+                        );
+                        return false;
+                    }
+                }
+                None => {
+                    panic!("Could not find item with name {}", order_item.name);
+                }
+            }
+        }
+
+        self.items = clone_inventory;
+        self.held.insert(order.id, order.items.clone());
+        true
+    }
+
+    fn ret_items(&mut self, order_id: &u8) {
+        for held_item in self.held.get(order_id).unwrap() {
+            match self.items.get_mut(&held_item.name) {
+                Some(item) => item.qty += held_item.qty,
+                None => {
+                    panic!("Could not find item with name {}", held_item.name);
+                }
+            }
+        }
+    }
+
+    fn log_inventory(&self) {
+        println!("\n| Current Inventory");
+        for (item_name, item) in &self.items {
+            println!("- {} has a qty of {}", item_name, item.qty);
+        }
+        println!("\n");
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let mut store_inventory: Inventory = Inventory {
+        held: HashMap::from([]),
         items: HashMap::from([
             (
                 "item1".to_string(),
@@ -76,11 +125,11 @@ async fn main() {
         },
     ];
 
-    log_inventory(&store_inventory);
+    store_inventory.log_inventory();
 
     for order in orders {
         proces_order(&mut store_inventory, order).await;
-        log_inventory(&store_inventory);
+        store_inventory.log_inventory();
     }
 
     match signal::ctrl_c().await {
@@ -91,88 +140,14 @@ async fn main() {
     }
 }
 
-fn log_inventory(inventory: &Inventory) {
-    println!("\n| Current Inventory");
-    for (item_name, item) in &inventory.items {
-        println!("– {} has a qty of {}", item_name, item.qty);
-    }
-    println!("\n");
-}
-
 async fn proces_order(inventory: &mut Inventory, order: Order) {
     println!("processing order #{}", order.id);
-    let taken_inventory = hold_inventory(inventory, &order);
-    if taken_inventory.items.len() > 0 {
+    if inventory.hold_items(&order) {
         if collect_payment(&order).await {
             println!("Sucessfully collected payment for order #{}", order.id);
         } else {
-            supply_inventory(inventory, &mut inventory_from_order(&order));
+            inventory.ret_items(&order.id);
             println!("Error while collecting payment for order #{}", order.id);
-        }
-    }
-}
-
-fn hold_inventory(inventory: &mut Inventory, order: &Order) -> Inventory {
-    let did_take = take_inventory(inventory, order);
-    if did_take {
-        inventory_from_order(order)
-    } else {
-        Inventory {
-            items: HashMap::from([]),
-        }
-    }
-}
-
-fn take_inventory(inventory: &mut Inventory, order: &Order) -> bool {
-    let mut taken_inventory = Inventory {
-        items: HashMap::from([]),
-    };
-
-    for order_item in &order.items {
-        match inventory.items.get_mut(&order_item.name) {
-            Some(inventory_item) => {
-                if inventory_item.qty >= order_item.qty {
-                    inventory_item.qty -= order_item.qty;
-                    taken_inventory
-                        .items
-                        .insert(order_item.name.clone(), order_item.clone());
-                } else {
-                    println!("| Returning inventory for order #{}. \n| {} has qty of {} but order is requesting qty of {}", 
-                        order.id, inventory_item.name, inventory_item.qty, order_item.qty);
-                    supply_inventory(inventory, &mut taken_inventory);
-                    return false;
-                }
-            }
-            None => {
-                panic!("Could not find inventory item named {}", order_item.name);
-            }
-        }
-    }
-
-    true
-}
-
-fn inventory_from_order(order: &Order) -> Inventory {
-    let mut new_inventory = Inventory {
-        items: HashMap::from([]),
-    };
-
-    for order_item in &order.items {
-        new_inventory
-            .items
-            .insert(order_item.name.clone(), order_item.clone());
-    }
-
-    new_inventory
-}
-
-fn supply_inventory(inventory: &mut Inventory, new_inventory: &mut Inventory) {
-    for (new_item_name, new_item) in &new_inventory.items {
-        match inventory.items.get_mut(new_item_name) {
-            Some(inventory_item) => inventory_item.qty += new_item.qty,
-            None => {
-                panic!("Could not find inventory item named {}", new_item_name);
-            }
         }
     }
 }
